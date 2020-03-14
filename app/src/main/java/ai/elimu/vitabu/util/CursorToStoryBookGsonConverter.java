cb4684daa4614b7743b487d2661d5af6e1663026
Nya Elimu <nya@elimu.ai>
2020-03-14 18:55:12 +0800

    Update CursorToStoryBookGsonConverter.java

--- app/src/main/java/ai/elimu/vitabu/util/CursorToStoryBookGsonConverter.java
+++ app/src/main/java/ai/elimu/vitabu/util/CursorToStoryBookGsonConverter.java
@@ -1,20 +1,30 @@
 package ai.elimu.vitabu.util;
 
 import android.database.Cursor;
+import android.util.Log;
+
+import java.util.Arrays;
 
 import ai.elimu.model.gson.content.StoryBookGson;
 
 public class CursorToStoryBookGsonConverter {
 
     public static StoryBookGson getStoryBook(Cursor cursor) {
-        int columnId = cursor.getColumnIndex("_id");
+        Log.i(CursorToStoryBookGsonConverter.class.getName(), "getStoryBook");
+
+        Log.i(CursorToStoryBookGsonConverter.class.getName(), "Arrays.toString(cursor.getColumnNames()): " + Arrays.toString(cursor.getColumnNames()));
+
+        int columnId = cursor.getColumnIndex("id");
         Long id = cursor.getLong(columnId);
+        Log.i(CursorToStoryBookGsonConverter.class.getName(), "id: " + id);
 
-        int columnTitle = cursor.getColumnIndex("TITLE");
+        int columnTitle = cursor.getColumnIndex("title");
         String title = cursor.getString(columnTitle);
+        Log.i(CursorToStoryBookGsonConverter.class.getName(), "title: \"" + title + "\"");
 
-        int columnDescription = cursor.getColumnIndex("DESCRIPTION");
+        int columnDescription = cursor.getColumnIndex("description");
         String description = cursor.getString(columnDescription);
+        Log.i(CursorToStoryBookGsonConverter.class.getName(), "description: \"" + description + "\"");
 
         StoryBookGson storyBook = new StoryBookGson();
         storyBook.setId(id);
