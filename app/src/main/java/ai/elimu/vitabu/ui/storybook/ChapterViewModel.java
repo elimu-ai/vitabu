ee7d5c7595581ba22337587f48a68ddfae89256a
Nya Elimu <nya@elimu.ai>
2020-04-14 09:17:36 +0800

    #2 Fetch chapters from StoryBook provider
    - Added images to view model

--- app/src/main/java/ai/elimu/vitabu/ui/storybook/ChapterViewModel.java
+++ app/src/main/java/ai/elimu/vitabu/ui/storybook/ChapterViewModel.java
@@ -8,21 +8,52 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import ai.elimu.model.gson.content.multimedia.ImageGson;
+
 public class ChapterViewModel extends ViewModel {
 
+    private List<ImageGson> images;
+
     private MutableLiveData<Integer> index = new MutableLiveData<>();
+
+    private LiveData<ImageGson> image = Transformations.map(index, new Function<Integer, ImageGson>() {
+        @Override
+        public ImageGson apply(Integer input) {
+            Log.i(getClass().getName(), "apply");
+
+            Log.i(getClass().getName(), "input: " + input);
+
+            // Lookup image based on chapter index
+            // TODO: use "input" variable to get the corresponding chapter
+            ImageGson imageGson = images.get(0);
+
+            return imageGson;
+        }
+    });
 
     private LiveData<String> text = Transformations.map(index, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
             Log.i(getClass().getName(), "apply");
 
+            Log.i(getClass().getName(), "input: " + input);
+
             return "Hello world from chapter: " + input;
         }
     });
 
+    public void setImages(List<ImageGson> images) {
+        this.images = images;
+    }
+
     public void setIndex(int index) {
         this.index.setValue(index);
+    }
+
+    public LiveData<ImageGson> getImage() {
+        return image;
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/ai/elimu/vitabu/ui/storybook/PlaceholderFragment.java
+++ app/src/main/java/ai/elimu/vitabu/ui/storybook/PlaceholderFragment.java
@@ -1,11 +1,17 @@
 package ai.elimu.vitabu.ui.storybook;
 
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,7 +19,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ai.elimu.model.gson.content.multimedia.ImageGson;
+import ai.elimu.vitabu.BuildConfig;
 import ai.elimu.vitabu.R;
+import ai.elimu.vitabu.util.CursorToImageGsonConverter;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -43,6 +55,33 @@ public class PlaceholderFragment extends Fragment {
             index = getArguments().getInt(ARG_CHAPTER_NUMBER);
         }
         chapterViewModel.setIndex(index);
+
+        List<ImageGson> images = new ArrayList<>();
+        Uri uri = Uri.parse("content://" + BuildConfig.CONTENT_PROVIDER_APPLICATION_ID + ".provider.image_provider/image/1");
+        Log.i(getClass().getName(), "uri: " + uri);
+        Cursor imageCursor = getContext().getContentResolver().query(uri, null, null, null, null);
+        if (imageCursor == null) {
+            Log.e(getClass().getName(), "imageCursor == null");
+            Toast.makeText(getContext(), "imageCursor == null", Toast.LENGTH_LONG).show();
+        } else {
+            Log.i(getClass().getName(), "imageCursor.getCount(): " + imageCursor.getCount());
+            if (imageCursor.getCount() == 0) {
+                Log.e(getClass().getName(), "imageCursor.getCount() == 0");
+            } else {
+                Log.i(getClass().getName(), "imageCursor.getCount(): " + imageCursor.getCount());
+
+                imageCursor.moveToFirst();
+
+                // Convert from database row to ImageGson object
+                ImageGson imageGson = CursorToImageGsonConverter.getImage(imageCursor);
+
+                imageCursor.close();
+                Log.i(getClass().getName(), "cursor.isClosed(): " + imageCursor.isClosed());
+
+                images.add(imageGson);
+            }
+        }
+        chapterViewModel.setImages(images);
     }
 
     @Override
@@ -50,13 +89,27 @@ public class PlaceholderFragment extends Fragment {
         Log.i(getClass().getName(), "onCreateView");
 
         View root = inflater.inflate(R.layout.fragment_storybook, container, false);
-        final TextView textView = root.findViewById(R.id.chapter_label);
-        chapterViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        final ImageView imageView = root.findViewById(R.id.chapter_image);
+        chapterViewModel.getImage().observe(getViewLifecycleOwner(), new Observer<ImageGson>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(ImageGson imageGson) {
+                Log.i(getClass().getName(), "onChanged");
+                byte[] bytes = imageGson.getBytes();
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                imageView.setImageBitmap(bitmap);
             }
         });
+
+        final TextView textView = root.findViewById(R.id.chapter_text);
+        chapterViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String text) {
+                Log.i(getClass().getName(), "onChanged");
+                textView.setText(text);
+            }
+        });
+
         return root;
     }
 }
