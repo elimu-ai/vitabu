5001031902e6934fb8ddeba5b8a94562caca31db
Nya Elimu <nya@elimu.ai>
2020-04-17 14:47:22 +0800

    #2 Fetch chapters from StoryBook provider
    - Display paragraph text.

--- app/src/main/java/ai/elimu/vitabu/ui/storybook/ChapterFragment.java
+++ app/src/main/java/ai/elimu/vitabu/ui/storybook/ChapterFragment.java
@@ -3,16 +3,19 @@ package ai.elimu.vitabu.ui.storybook;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import ai.elimu.model.gson.v2.content.StoryBookChapterGson;
+import ai.elimu.model.gson.v2.content.StoryBookParagraphGson;
 import ai.elimu.vitabu.R;
 
 public class ChapterFragment extends Fragment {
@@ -57,9 +60,17 @@ public class ChapterFragment extends Fragment {
 
         Log.i(getClass().getName(), "storyBookChapter.getStoryBookParagraphs(): " + storyBookChapter.getStoryBookParagraphs());
         if (storyBookChapter.getStoryBookParagraphs() != null) {
-            // TODO
-//            TextView textView = root.findViewById(R.id.chapter_text);
-//            textView.setText(chapterText);
+            String chapterText = "";
+            for (StoryBookParagraphGson storyBookParagraphGson : storyBookChapter.getStoryBookParagraphs()) {
+                Log.i(getClass().getName(), "storyBookParagraphGson.getOriginalText(): \"" + storyBookParagraphGson.getOriginalText() + "\"");
+                if (!TextUtils.isEmpty(chapterText)) {
+                    chapterText += "\n\n";
+                }
+                chapterText += storyBookParagraphGson.getOriginalText();
+            }
+
+            TextView textView = root.findViewById(R.id.chapter_text);
+            textView.setText(chapterText);
         }
 
         return root;
