6ad50af405aeefc7eb8e9b0f473d3a0dd03d3ccd
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-05 15:46:31 +0700

    Convert MainActivity code from Java to Kotlin

--- app/src/main/java/ai/elimu/vitabu/MainActivity.kt
+++ app/src/main/java/ai/elimu/vitabu/MainActivity.kt
@@ -1,34 +1,32 @@
-package ai.elimu.vitabu;
+package ai.elimu.vitabu
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
+import ai.elimu.vitabu.ui.StoryBooksActivity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
 
-import ai.elimu.vitabu.ui.StoryBooksActivity;
+    private val TAG = MainActivity::class.java.simpleName
 
-public class MainActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.i(getClass().getName(), "onCreate");
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
 
         // Verify that the content-provider APK has been installed
         // TODO
     }
 
-    @Override
-    protected void onStart() {
-        Log.i(getClass().getName(), "onStart");
-        super.onStart();
+    override fun onStart() {
+        Log.i(TAG, "onStart")
+        super.onStart()
 
-        Intent intent = new Intent(this, StoryBooksActivity.class);
-        startActivity(intent);
+        val intent = Intent(this, StoryBooksActivity::class.java)
+        startActivity(intent)
 
-        finish();
+        finish()
     }
 }
