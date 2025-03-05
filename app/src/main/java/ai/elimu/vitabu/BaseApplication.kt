13efb8d40ebff9d1512ff86a38970e4a240418ab
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-05 16:57:38 +0700

    Convert BaseApplication code to Kotlin

--- app/src/main/java/ai/elimu/vitabu/BaseApplication.kt
+++ app/src/main/java/ai/elimu/vitabu/BaseApplication.kt
@@ -1,45 +1,33 @@
-package ai.elimu.vitabu;
+package ai.elimu.vitabu
 
-import android.app.Application;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
+import android.app.Application
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.util.Log
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+class BaseApplication : Application() {
 
-public class BaseApplication extends Application {
+    var tts: TextToSpeech? = null
+        private set
 
-    public static final float SPEECH_RATE = 0.5f;
-
-    Executor executorService = Executors.newSingleThreadExecutor();
-
-    private TextToSpeech tts;
-
-    @Override
-    public void onCreate() {
-        Log.i(getClass().getName(), "onCreate");
-        super.onCreate();
+    override fun onCreate() {
+        Log.i(javaClass.name, "onCreate")
+        super.onCreate()
 
         // Initialize the Text-to-Speech (TTS) engine
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                Log.i(getClass().getName(), "onInit");
+        tts = TextToSpeech(applicationContext, object : OnInitListener {
+            override fun onInit(status: Int) {
+                Log.i(javaClass.name, "onInit")
 
                 // Fetch the chosen language from the Appstore
                 // TODO
 //                tts.setLanguage(new Locale("hin"));
-
-                tts.setSpeechRate(SPEECH_RATE);
+                tts!!.setSpeechRate(SPEECH_RATE)
             }
-        });
+        })
     }
 
-    public TextToSpeech getTTS() {
-        return tts;
-    }
-
-    public Executor getExecutor() {
-        return executorService;
+    companion object {
+        const val SPEECH_RATE: Float = 0.5f
     }
 }
