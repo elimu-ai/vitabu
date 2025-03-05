c65ceac39bed76b76f7d3804e5868020320ffecc
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-05 15:52:30 +0700

    Migrate to using viewBinding in MainActivity

--- app/src/main/java/ai/elimu/vitabu/MainActivity.kt
+++ app/src/main/java/ai/elimu/vitabu/MainActivity.kt
@@ -1,5 +1,6 @@
 package ai.elimu.vitabu
 
+import ai.elimu.vitabu.databinding.ActivityMainBinding
 import ai.elimu.vitabu.ui.StoryBooksActivity
 import android.content.Intent
 import android.os.Bundle
@@ -9,12 +10,14 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.java.simpleName
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.i(TAG, "onCreate")
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Verify that the content-provider APK has been installed
         // TODO
