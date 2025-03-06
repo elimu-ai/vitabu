5e78f907fd89429dae901ca101b1df251392c6c6
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-06 11:43:15 +0700

    Convert SingleClickListener from Java to Kotlin

--- app/src/main/java/ai/elimu/vitabu/util/SingleClickListener.kt
+++ app/src/main/java/ai/elimu/vitabu/util/SingleClickListener.kt
@@ -1,27 +1,26 @@
-package ai.elimu.vitabu.util;
+package ai.elimu.vitabu.util
 
-import android.os.SystemClock;
-import android.view.View;
-
+import android.os.SystemClock
+import android.view.View
 
 /**
  * Prevent double click
  */
-public abstract class SingleClickListener implements View.OnClickListener {
-
-    private final static long DOUBLE_CLICK_INTERVAL = 2000;
-
-    private static long mLastClickTime = 0;
-
-    @Override
-    final public void onClick(View v) {
-        if (SystemClock.elapsedRealtime() - mLastClickTime < DOUBLE_CLICK_INTERVAL){
-            return ;
+abstract class SingleClickListener : View.OnClickListener {
+    override fun onClick(v: View) {
+        if (SystemClock.elapsedRealtime() - mLastClickTime < DOUBLE_CLICK_INTERVAL) {
+            return
         }
-        mLastClickTime = SystemClock.elapsedRealtime();
+        mLastClickTime = SystemClock.elapsedRealtime()
 
-        onSingleClick(v);
+        onSingleClick(v)
     }
 
-    public abstract void onSingleClick(View v);
+    abstract fun onSingleClick(v: View?)
+
+    companion object {
+        private const val DOUBLE_CLICK_INTERVAL: Long = 2000
+
+        private var mLastClickTime: Long = 0
+    }
 }
