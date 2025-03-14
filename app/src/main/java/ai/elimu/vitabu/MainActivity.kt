5da4bbdd17186dd0552e205a1e3966641bb73b66
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-14 11:25:02 +0700

    Check for Content Provider existence before launching

--- app/src/main/java/ai/elimu/vitabu/MainActivity.kt
+++ app/src/main/java/ai/elimu/vitabu/MainActivity.kt
@@ -3,8 +3,11 @@ package ai.elimu.vitabu
 import ai.elimu.vitabu.databinding.ActivityMainBinding
 import ai.elimu.vitabu.ui.StoryBooksActivity
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -18,18 +21,45 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Verify that the content-provider APK has been installed
-        // TODO
     }
 
     override fun onStart() {
         Log.i(TAG, "onStart")
         super.onStart()
 
-        val intent = Intent(this, StoryBooksActivity::class.java)
-        startActivity(intent)
+        if (isContentProviderInstalled()) {
+            val intent = Intent(this, StoryBooksActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
-        finish()
+    private fun isContentProviderInstalled(): Boolean {
+        try {
+            val packageInfoAppstore: PackageInfo =
+                packageManager.getPackageInfo(BuildConfig.CONTENT_PROVIDER_APPLICATION_ID, 0)
+            Log.i(TAG, "packageInfoAppstore.versionCode: " + packageInfoAppstore.versionCode)
+            return true
+        } catch (e: PackageManager.NameNotFoundException) {
+            Log.e(TAG,"getPackageInfo exception: " + e.message)
+            AlertDialog.Builder(this)
+                .setMessage(resources.getString(R.string.content_provider_needed))
+                .setPositiveButton(getString(R.string.install)
+                ) { _, _ ->
+                    val openProviderIntent = Intent().apply {
+                        setClassName(BuildConfig.APPSTORE_APPLICATION_ID,
+                            "ai.elimu.appstore.MainActivity")
+                    }
+                    try {
+                        startActivity(openProviderIntent)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        Log.e(TAG, "startActivity exception: " + e.message)
+                    }
+                }
+                .setCancelable(false)
+                .create().show()
+            return false
+        }
     }
 }
