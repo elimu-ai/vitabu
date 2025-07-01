18b7a3e6cafd0587323f6a951ce63e3ccd07976c
tuancoltech <tuancoltech@gmail.com>
2025-07-01 22:39:33 +0700

    Adjust book reading speech rate

--- app/src/main/java/ai/elimu/vitabu/util/ReadingLevelExtension.kt
+++ app/src/main/java/ai/elimu/vitabu/util/ReadingLevelExtension.kt
@@ -4,10 +4,10 @@ import ai.elimu.model.v2.enums.ReadingLevel
 
 fun ReadingLevel?.toSpeechRate(): Float {
     return when (this) {
-        ReadingLevel.LEVEL1 -> 0.4f
-        ReadingLevel.LEVEL2 -> 0.6f
-        ReadingLevel.LEVEL3 -> 0.8f
-        ReadingLevel.LEVEL4 -> 1.0f
-        null -> 1.0f
+        ReadingLevel.LEVEL1 -> 0.2f
+        ReadingLevel.LEVEL2 -> 0.3f
+        ReadingLevel.LEVEL3 -> 0.4f
+        ReadingLevel.LEVEL4 -> 0.5f
+        null -> 0.5f
     }
 }
