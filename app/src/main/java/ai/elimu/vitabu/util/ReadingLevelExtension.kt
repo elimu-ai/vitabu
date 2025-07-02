7a5f76cfb18849c266ad4cfda256a1f80411269a
Tuan Nguyen <tuancoltech@gmail.com>
2025-07-02 09:39:38 +0700

    Slower the speech rate for all levels
    Co-authored-by: Jo G. 🌏 <1451036+jo-elimu@users.noreply.github.com>

--- app/src/main/java/ai/elimu/vitabu/util/ReadingLevelExtension.kt
+++ app/src/main/java/ai/elimu/vitabu/util/ReadingLevelExtension.kt
@@ -4,10 +4,10 @@ import ai.elimu.model.v2.enums.ReadingLevel
 
 fun ReadingLevel?.toSpeechRate(): Float {
     return when (this) {
-        ReadingLevel.LEVEL1 -> 0.2f
-        ReadingLevel.LEVEL2 -> 0.3f
-        ReadingLevel.LEVEL3 -> 0.4f
-        ReadingLevel.LEVEL4 -> 0.5f
-        null -> 0.5f
+        ReadingLevel.LEVEL1 -> 0.04f
+        ReadingLevel.LEVEL2 -> 0.08f
+        ReadingLevel.LEVEL3 -> 0.16f
+        ReadingLevel.LEVEL4 -> 0.40f
+        null -> 0.40f
     }
 }
